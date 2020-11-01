ad9e1af85f1369870fa526b6d715d4bcd0735cbc
Arfiyan19 <arfieyan1903@gmai.com>
2020-11-01 19:46:25 +0700

    commit1

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="jumbotron">
+        <h1 class="display-4">ARFIYAN WAHYU PRATAMA</h1>
+        <p class="lead">SINAU VUE MUDAH </p>
+        <hr class="my-4">
+        <router-link :to="{ name: 'about' }" class="btn btn-primary btn-lg" role="button">SELENGKAPNYA</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+
+
+    }
+</script>
